a4ba927a50d3f4d9483438d497f9e5b0d538d8f7
unknown <valerde88@outlook.com>
2026-04-16 10:01:36 -0300

    eurro de modulo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import prompt from "prompt"
+import prompt from "prompt";
+import { mainPrompt } from "./prompts/prompt-main";
 
-async function main (){
-    prompt.
+async function main() {
+  prompt.get(mainPrompt, async (err, result) => {
+    if (result.select == 1) console.log("Escolheu o qrCode");
+    else if (result.select == 2) console.log("Escolheu o prompt");
+  });
+  prompt.start();
 }
 
-main()
+main();
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,11 +1,13 @@
 import { message } from "prompt";
 
 const mainPrompt = [
-    {
-        name:"select",
-        description:"Escolha a Ferramenta(1 - QRCODE ou (2- PASSWORD",
-        pattern:/^[1-2]+$/,
-        message:"Escolha apenas entre 1 e 2",
-        required:true
-    }
-]
+  {
+    name: "select",
+    description: "Escolha a Ferramenta(1 - QRCODE ou (2- PASSWORD",
+    pattern: /^[1-2]+$/,
+    message: "Escolha apenas entre 1 e 2",
+    required: true,
+  },
+];
+
+export default mainPrompt;
