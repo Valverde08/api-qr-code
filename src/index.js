bc1749ebb8f6be1afda33a622fccf8048103adad
Vinicius Valverde da Silva Cardoso <viniciuscardoso@uneb.br>
2026-04-17 17:32:31 -0300

    Atualizacao

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import prompt from "prompt";
 import  mainPrompt  from "./prompts/prompt-main.js";
+import createQrCode from "./services/qr-code/create.js";
 
 async function main() {
   
 
   prompt.get(mainPrompt, function(err, result){
-    if (result.select == 1) console.log("Escolheu o qrCode");
+    if (result.select == 1) await createQrCode()
     else if (result.select == 2) console.log("Escolheu o prompt");
   });
 
