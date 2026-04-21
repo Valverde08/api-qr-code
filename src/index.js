ccf8ec18484186b907be2da19dbd739ddf5ed296
unknown <valerde88@outlook.com>
2026-04-21 11:07:06 -0300

    Finalizacao de projeto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import prompt from "prompt";
-import  mainPrompt  from "./prompts/prompt-main.js";
+import mainPrompt from "./prompts/prompt-main.js";
 import createQrCode from "./services/qr-code/create.js";
+import creatPassword from "./services/password/create-password.js";
 
 async function main() {
-  
-
-  prompt.get(mainPrompt, function(err, result){
-    if (result.select == 1) await createQrCode()
-    else if (result.select == 2) console.log("Escolheu o prompt");
+  prompt.get(mainPrompt, async function (err, result) {
+    if (result.select == 1) createQrCode();
+    else if (result.select == 2) await creatPassword();
   });
 
   prompt.start();
--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,7 +1,19 @@
 import chalk from "chalk";
 
-const promptQrCode =[
-
-]
+const promptQrCode = [
+  {
+    name: "link",
+    description: chalk.yellow("Digiteo link para gerar o QR COde"),
+  },
+  {
+    name: "type:",
+    description: chalk.yellow(
+      "Escolha entre o tipo(1 - NORMAL ou (-2 TERMINAL",
+    ),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Escolhe apenas entre 1 e 2"),
+    required: true,
+  },
+];
 
 export default promptQrCode;
